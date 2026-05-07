f0e1bb27c6e361384d300ba4b01fbcc7e2a197dc
Taylor Viti <sinkoman@gmail.com>
2026-05-06 21:44:33 -0700

    Check in Emacs directory local variables
    Update task tracker

---
+++ .dir-locals.el
@@ -0,0 +1,22 @@
+((nil . ((fill-column . 80)))
+ ;; (julia-mode . ((eglot-server-programs . ((julia-mode  . ("julia" "-e" "using LanguageServer; runserver()" "/home/taylor/Source/bunkalunk/src/julia"))))))
+ (julia-mode . ((eglot-ignored-server-capabilities . (:inlayHintProvider))))
+ (python-mode . ((compile-command . "pytest")
+		 (python-shell-interpreter . "ipython")
+		 (python-shell-interpreter-args . "--simple-prompt")
+		 (eglot-workspace-configuration
+		  . (:pylsp
+		     (:plugins
+		      ;; Disable built-in providers that overlap with ruff
+		      (:pycodestyle (:enabled :json-false)
+		       :pyflakes    (:enabled :json-false)
+		       :mccabe      (:enabled :json-false)
+		       :autopep8    (:enabled :json-false)
+		       :yapf        (:enabled :json-false)
+		       ;; rope: refactoring
+		       :rope (:enabled t)
+		       ;; ruff: linting + formatting
+		       :ruff        (:enabled t :formatEnabled t)
+		       ;; mypy: type checking
+		       :pylsp_mypy  (:enabled t
+					      :args ["--config-file" "src/python/pyproject.toml"]))))))))
