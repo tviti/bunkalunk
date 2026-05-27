d150128f4ad2c0b2fa4e83de4b6e5ab6765e1c5a
Taylor Viti <sinkoman@gmail.com>
2026-05-27 01:16:01 -0700

    Wait for julia auto-formatter to finish
    Add a loop with timeout that waits for eglot-format-buffer to complete. Runic
    tends to undo buffer edits without this.

--- src/julia/.dir-locals.el
+++ src/julia/.dir-locals.el
@@ -1,2 +1,6 @@
 ((julia-mode . ((eglot-ignored-server-capabilities . (:inlayHintProvider))
-                (eval . (add-hook 'before-save-hook #'eglot-format-buffer nil 'local)))))
+                (eval . (add-hook 'before-save-hook
+				  (lambda ()
+				    (eglot-format-buffer)
+				    (while (accept-process-output nil 0.5)))
+				  nil 'local)))))
