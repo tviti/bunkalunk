87bdd8b0b19f31fa667461850daf0ff7d93d8e58
Taylor Viti <sinkoman@gmail.com>
2026-06-07 03:15:03 -0700

    Cleanup unused nix-shell derivations; update Manifest.toml

--- shell.nix
+++ shell.nix
@@ -2,61 +2,11 @@
 let
   pkgs = import <nixpkgs> {};
 
-  myPython = pkgs.python3.override {
-    packageOverrides = self: super: {
-      # Provide ua-generator in the package set via an override.
-      ua_generator = super.buildPythonPackage rec {
-        pname = "ua-generator";
-        version = "2.0.24";
-        src = pkgs.fetchFromGitHub {
-          owner = "iamdual";
-          repo = "ua-generator";
-          rev = "2.0.24";
-          sha256 = "sha256-eqrstqwbXw3KVaTmBCXAwDQ++IC4pSVqye3tmmnIKe0=";
-        };
-        # Use legacy setuptools build via pyproject
-        pyproject = true;
-        nativeBuildInputs = with pkgs.python3Packages; [ setuptools ];
-        propagatedBuildInputs = with pkgs.python3Packages; [];
-        pythonImportsCheck = [ "ua_generator" ];
-      };
-
-      garminconnect = super.garminconnect.overridePythonAttrs (old: {
-        src = pkgs.fetchFromGitHub {
-          owner = "cyberjunky";
-          repo = "python-garminconnect";
-          rev = "0.3.1";
-          sha256 = "sha256-31zCAJAr3f1nevd61a3dCEZ6vjPhW0uLHhnnAUG03Yc=";
-        };
-
-        dependencies = (with pkgs.python3Packages; [
-          curl-cffi
-          requests
-          self.ua_generator
-        ]);
-      });
-
-    };
-  };
-  julia-fhs = pkgs.buildFHSEnv {
-    name = "julia";
-    targetPkgs = pkgs: with pkgs; [
-      julia-bin
-      git
-      zlib
-      glib
-      stdenv.cc.cc.lib
-      sqlite
-      gdal
-      # List all system libraries your Julia packages might try to link against
-    ];
-    runScript = "julia";
-  };
+  myPython = pkgs.python3;
   pythonEnv = myPython.withPackages (ps: with ps; [
     requests
     fitdecode
     h5py
-    # garminconnect
 
     ipython
     pytest
@@ -81,7 +31,7 @@ pkgs.mkShell {
                 ++ (with pkgs;
                   [
                     ruff
-                    # julia-fhs
+                    sqlite
                   ]);
 
   TMPDIR = "/tmp";  # Julia tries (and fails) to write tmpdata to nix-store without this
